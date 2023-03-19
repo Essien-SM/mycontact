c5e1cb54827ff580485fa33f0ee563137da901d4
Essien-SM <stephenyessien@gmail.com>
2023-03-19 13:12:16 +0000

    made component with the mapping method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function CreateCard(contact) {
+  return <Card 
+  key={contact.id} 
+  name={contact.name}
+  img={contact.imgURL}
+  tel={contact.phone}
+  email={contact.email}
+   />;
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card 
+      <Avatar img="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80" />
+
+      {contacts.map(CreateCard)}
+
+      {/* <Card 
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -26,9 +41,8 @@ function App() {
         tel={contacts[2].phone}
         email={contacts[2].email}
 
-      />
+      /> */}
     </div>
-    
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,22 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
-
-function Card(props){
-    return(
-        <div>
-            
-            <div className="card">
-            <div className="top">
-            <h2 className="name">{props.name}</h2>
-            <img
-                className="circle-img"
-                src={props.img}
-                alt="avatar_img"
-            />
-            </div>
-            <div className="bottom">
-            <p className="info">{props.tel}</p>
-            <p className="info">{props.email}</p>
-            </div>
+function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <Avatar img={props.img} />
+        </div>
+        <div className="bottom">
+          <Detail detailInfo={props.email} />
+          <Detail detailInfo={props.tel} />
+        </div>
       </div>
-    
     </div>
-    )
+  );
 }
 
-export default Card
+export default Card;
